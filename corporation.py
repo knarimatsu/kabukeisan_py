c479e2fe393b02704f14e4bb49fff4187ad6c3a0
knarimatsu <nariomasahisa@gmail.com>
2023-08-26 20:55:43 +0900

    クラス化、企業価値に関する計算の追加

---
+++ corporation.py
@@ -0,0 +1,14 @@
+class Corporation():
+    
+    def __init__(self) -> None:
+        # 流動資産
+        self.current_asset = 0
+        # 流動負債
+        self.current_liability = 0
+        # 資産
+        # 負債
+        # 有利子負債
+        
+        pass
+    
+    
