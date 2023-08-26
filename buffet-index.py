7b0d80f39f4b4f66cc2da6baaa31eb562d0187c8
knarimatsu <nariomasahisa@gmail.com>
2023-08-26 19:52:23 +0900

    バフェット指数の計算

---
+++ buffet-index.py
@@ -0,0 +1,20 @@
+
+def buffet_index(total_market_cap:int, nominal_gdp:int) -> str:
+    """
+    バフェット指数
+    引数
+        該当国総時価総額: int
+        該当国の名目GDP: int
+    戻り値
+        割高/割安: String
+    """
+    
+    try:
+        result = total_market_cap / nominal_gdp
+        if result >= 1:
+            return "割高"
+        else:
+            return "割安"
+    except ZeroDivisionError:
+        result = "nominal_gdp should be over 0"
+        return result
