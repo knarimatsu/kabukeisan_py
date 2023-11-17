3bf6dc658c5044cc53de132768b83306a1cc59f1
knarimatsu <nariomasahisa@gmail.com>
2023-11-18 01:54:07 +0900

    文字列に変更

--- calc_value.py
+++ calc_value.py
@@ -10,8 +10,11 @@ value = 0
 
 for i in range(1, 2705):
     value += flow / (1 + interest) ** i
-print('-------------------')
-print('| {} |'.format(int(round(value, 0))))
-print('| {} |'.format(round((buy_price / value), 2) ))
-print('| {} |'.format(round((value / buy_price ), 2) ))
-print('-------------------')
+print("-------------------")
+print("| {} |".format(int(round(value, 0))))
+print("| 時価総額 / 企業価値{} |".format(round((buy_price / value), 2)))
+if (buy_price / value) < 1:
+    print("| 割安 |")
+else:
+    print("| 割高 |")
+print("-------------------")
