1dd44363c15be24d33fce43adca5bb5054e0ce44
knarimatsu <nariomasahisa@gmail.com>
2023-11-18 01:55:59 +0900

    文字列に変更

--- calc_value.py
+++ calc_value.py
@@ -12,7 +12,7 @@ for i in range(1, 2705):
     value += flow / (1 + interest) ** i
 print("-------------------")
 print("| {} |".format(int(round(value, 0))))
-print("| 時価総額 / 企業価値{} |".format(round((buy_price / value), 2)))
+print("| 時価総額 / 企業価値 = {} |".format(round((buy_price / value), 2)))
 if (buy_price / value) < 1:
     print("| 割安 |")
 else:
