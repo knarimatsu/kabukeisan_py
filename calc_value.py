b750494cf825584d9742dc12a2307b3be11766fd
knarimatsu <nariomasahisa@gmail.com>
2023-11-19 23:45:38 +0900

    マイナスの価値は考慮に入れない

--- calc_value.py
+++ calc_value.py
@@ -13,10 +13,14 @@ for i in range(1, 2705):
 print("-------------------")
 print("| {} |".format(int(round(value, 0))))
 print("| 時価総額 / 企業価値 = {} |".format(round((buy_price / value), 2)))
-if (buy_price / value) < 0.66:
-    print("| 割安 |")
-elif (buy_price / value) < 1:
-    print("| 少し |")
+if value >= 0:
+    if (buy_price / value) < 0.66:
+        print("| 割安 |")
+    elif (buy_price / value) < 1:
+        print("| 少し |")
+
+    else:
+        print("| 割高 |")
 else:
-    print("| 割高 |")
+    print("| マイナス |")
 print("-------------------")
