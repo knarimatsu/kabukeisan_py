9666baa03b282c0e11dfaa7c5906f7c1757ca4c1
knarimatsu <nariomasahisa@gmail.com>
2023-11-19 19:53:07 +0900

    安全域の概念の追加

--- calc_value.py
+++ calc_value.py
@@ -13,8 +13,10 @@ for i in range(1, 2705):
 print("-------------------")
 print("| {} |".format(int(round(value, 0))))
 print("| 時価総額 / 企業価値 = {} |".format(round((buy_price / value), 2)))
-if (buy_price / value) < 1:
+if (buy_price / value) < 0.66:
     print("| 割安 |")
+elif (buy_price / value) < 1:
+    print("| 少し |")
 else:
     print("| 割高 |")
 print("-------------------")
